e5265874fdae606f1c15040ac717d7bd4ef93642
Chris Dhanaraj <adhanaraj@gmail.com>
2016-03-08 20:20:20 -0600

    bring in new actions

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,4 +1,7 @@
 import fetch from 'isomorphic-fetch';
+import { checkHttpStatus, parseJSON } from '../utils';
+import { browserHistory } from 'react-router';
+import jwtDecode from 'jwt-decode';
 
 export const REQUEST_CURRENT_SHOW = 'REQUEST_CURRENT_SHOW';
 export const RECEIVE_CURRENT_SHOW = 'RECEIVE_CURRENT_SHOW';
@@ -19,9 +22,7 @@ export function requestCurrentShow() {
 export function receiveCurrentShow(json) {
   return {
     type: RECEIVE_CURRENT_SHOW,
-    data: json.data.children.map(child => {
-      return child.data;
-    }),
+    data: json,
     receivedAt: Date.now(),
   };
 }
@@ -60,5 +61,128 @@ export function fetchCurrentShowIfNeeded() {
     }
 
     return Promise.resolve();
+  };
+}
+
+export const LOGIN_USER_REQUEST = 'LOGIN_USER_REQUEST';
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
+export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const FETCH_PROTECTED_DATA_REQUEST = 'FETCH_PROTECTED_DATA_REQUEST';
+export const RECEIVE_PROTECTED_DATA = 'RECEIVE_PROTECTED_DATA'
+
+export function loginUserSuccess(token) {
+  localStorage.setItem('token', token);
+  return {
+    type: LOGIN_USER_SUCCESS,
+    payload: {
+      token,
+    },
+  };
+}
+
+export function loginUserFailure(error) {
+  localStorage.removeItem('token');
+  return {
+    type: LOGIN_USER_FAILURE,
+    payload: {
+      status: error.response.status,
+      statusText: error.response.statusText,
+    },
+  };
+}
+
+export function loginUserRequest() {
+  return {
+    type: LOGIN_USER_REQUEST,
+  };
+}
+
+export function logout() {
+  localStorage.removeItem('token');
+  return {
+    type: LOGOUT_USER,
+  };
+}
+
+export function logoutAndRedirect() {
+  return (dispatch, state) => {
+    dispatch(logout());
+    dispatch(browserHistory('/login'));
+  };
+}
+
+export function loginUser(email, password, redirect = '/') {
+  return (dispatch) => {
+    dispatch(loginUserRequest());
+    return fetch('/auth/getToken/', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+    .then(checkHttpStatus)
+    .then(parseJSON)
+    .then(response => {
+      try {
+        let decoded = jwtDecode(response.token);
+        dispatch(loginUserSuccess(response.token));
+        dispatch(browserHistory.push(redirect));
+      } catch (e) {
+        dispatch(loginUserFailure({
+          response: {
+            status: 403,
+            statusText: 'Invalid token',
+          },
+        }));
+      }
+    })
+    .catch(error => {
+      dispatch(loginUserFailure(error));
+    });
+  };
+}
+
+export function receiveProtectedData(data) {
+  return {
+    type: RECEIVE_PROTECTED_DATA,
+    payload: {
+      data,
+    },
+  };
+}
+
+export function fetchProtectedDataRequest() {
+  return {
+    type: FETCH_PROTECTED_DATA_REQUEST,
+  };
+}
+
+export function fetchProtectedData(token) {
+  return (dispatch, state) => {
+    dispatch(fetchProtectedDataRequest());
+    return fetch('http://localhost:3000/getData/', {
+      credentials: 'include',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(checkHttpStatus)
+    .then(parseJSON)
+    .then(response => {
+      dispatch(receiveProtectedData(response.data));
+    })
+    .catch(error => {
+      if (error.response.status === 401) {
+        dispatch(loginUserFailure(error));
+        dispatch(browserHistory.push('/login'));
+      }
+    });
   };
 }
